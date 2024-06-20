30e7ddc8fbc1289fae3d698a9ef2b6e9ba61c2ce
skricode <skricode@gmail.com>
2024-06-20 16:02:59 +0530

    CRUD functionalities created

---
+++ src/Empl.js
@@ -0,0 +1,21 @@
+const Data = [
+    {
+        id:1,
+        firstName:'Harsh',
+        lastName:'Vardhan',
+        age:28
+    },
+    {
+        id:2,
+        firstName:'Vinit',
+        lastName:'Pol',
+        age:29
+    },{
+        id:3,
+        firstName:'Hari',
+        lastName:'Vara',
+        age:26
+    },
+]
+
+export default Data;
